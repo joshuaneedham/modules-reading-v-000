554534ee9b7687b63d67bcc71129201e98102264
Joshua Needham <joshuabneedham@gmail.com>
2017-11-20 20:02:42 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,11 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  extend MetaDancing
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
+# 
+#   attr_accessor :name
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# end
