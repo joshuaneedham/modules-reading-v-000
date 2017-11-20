d6c6359fb16b2296c76a40e2277ebea0f14aab3d
Joshua Needham <joshuabneedham@gmail.com>
2017-11-20 20:02:02 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,9 +6,9 @@ class Dancer
   include FancyDance::InstanceMethods
 end
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#   end
+# end
